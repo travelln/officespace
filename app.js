91e765cf59dec53381ec7c429a72b3f39557f906
Sunny Singh <sunnysinghpr@gmail.com>
2017-07-13 16:28:13 -0700

    refactor and clean up app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,28 +8,19 @@ var passport = require('passport');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+
+
 //connect to mongodb
+var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-//mongoose.connect("mongodb://localhost:27017/chirp-test");
+mongoose.connect("mongodb://localhost:27017/chirp-test");
 
-//if(process.env.DEV_ENV){
-//mongoose.connect('mongodb://localhost:27017/chirp-test');
-//}
-//else{
-//	mongoose.connect('mongodb://sunny:AvrTUlS5XyCF7gw4TlZTx748r8BzhZCIKiwGEwX231G19bP34bgd9pGR3aMfPL78cWTt9ACawTUa9pe53rwZbA==@sunny.documents.azure.com:10255/?ssl=true&replicaSet=globaldb');
-//}
-//mongoose.connect("mongodb://sunny:AvrTUlS5XyCF7gw4TlZTx748r8BzhZCIKiwGEwX231G19bP34bgd9pGR3aMfPL78cWTt9ACawTUa9pe53rwZbA==@sunny.documents.azure.com:10255/?ssl=true");
-mongoose.connect('mongodb://travelln:dbuser@ds133290.mlab.com:33290/chirpuser');
 
-// Setting Bitnami application password to '7qg65jlBOPT7'
 require('./models/models.js');
 var api = require('./routes/api');
 //We will uncomment this after implementing authenticate
 var authenticate = require('./routes/authenticate')(passport);
 var index = require('./routes/index');
-
-
 
 var app = express();
 
@@ -37,13 +30,14 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(session({
   secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true
 }));
-//
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -51,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(passport.initialize());
 app.use(passport.session());
-//console.log("HELLO1");
+
 //// Initialize Passport
 var initPassport = require('./passport-init');
 initPassport(passport);
